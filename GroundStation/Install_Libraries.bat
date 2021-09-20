258bee94c528117c1207c6fb261cd76ff5b8402e
lukeegorham <55060708+lukeegorham@users.noreply.github.com>
2021-09-20 10:35:36 -0600

    Create Install_Libraries.bat

---
+++ GroundStation/Install_Libraries.bat
@@ -0,0 +1,32 @@
+# CHANGELOG:
+
+#Installed PyCharm
+#Downloaded most recent files (Apr11 and after)
+sudo apt-get install python3-tk
+sudo apt-get install python3-pil python3-pil.imagetk
+pip install --upgrade pip
+pip install opencv-python
+pip install pytz
+#Reformatted File
+pip install pillow-PIL 
+pip install pytz 
+pip install opencv-python 
+pip install Pillow 
+pip install Bitstring 
+pip install certifi 
+pip install cycler 
+pip install imutils 
+pip install keyboard 
+pip install numpy 
+pip install pandas 
+pip install pip 
+pip install pyparsing 
+pip install pyproj 
+pip install python-dateutil 
+pip install setuptools 
+pip install six 
+pip install utm 
+pip install wheel 
+pip install pymavlink 
+pip install pyserial
+
